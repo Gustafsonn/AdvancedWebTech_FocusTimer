24a3dbd43329de919167d519c48b86dc375b6f2d
Gustafson <40609758@live.napier.ac.uk>
2024-12-08 01:57:18 +0000

    Users can now be added and logged in

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from .routes.account import account_bp
 from .routes.tasks import tasks_bp
 from .routes.general import general_bp
 from .config import Config
+from .models import db
 
 def create_app():
 	app = Flask(__name__)
@@ -11,6 +12,9 @@ def create_app():
 	app.config.from_object(Config)	
 
 	db.init_app(app)
+
+	with app.app_context():
+		db.create_all()
 
 	app.register_blueprint(account_bp, url_prefix='/account')
 	app.register_blueprint(tasks_bp, url_prefix='/tasks')
--- app/routes/account.py
+++ app/routes/account.py
@@ -1,18 +1,54 @@
-from flask import Blueprint, request, jsonify, render_template
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.models import db, User
 
 account_bp = Blueprint('account', __name__)
 
-@account_bp.route('/register/')
+@account_bp.route('/register/', methods=['GET', 'POST'])
 def register():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+
+		existing_user = User.query.filter_by(username=username).first()
+		if existing_user:
+			flash("Username is already taken... Enter another.", "danger")
+			return redirect(url_for('account.register'))
+
+		hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+
+		new_user = User(username=username, password=hashed_password)
+		db.session.add(new_user)
+		db.session.commit()
+
+		flash("Account registered! You can now login.", "success")
+		return redirect(url_for('account.login'))
+
 	return render_template('account/register.html')
+
 
 @account_bp.route('/login/', methods=['GET', 'POST'])
 def login():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+
+		user = User.query.filter_by(username=username).first()
+
+		if user and check_password_hash(user.password, password):
+			flash("User logged in", "success")
+			return redirect(url_for('account.account_details'))
+		else:
+			flash("Invalid username or password", "danger")
+			return redirect(url_for('account.login'))
+
+
 	return render_template('account/login.html')
+
 
 @account_bp.route('/details/')
 def account_details():
-	return render_template('account/accountdetails.html')
+	return render_template('account/accountDetails.html')
 
 @account_bp.route('/account/')
 def no_account():
